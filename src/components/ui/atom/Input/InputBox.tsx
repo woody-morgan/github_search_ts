2142f1e878085924020cf5f08d8d400561d811b3
woody-morgan <woody.morgan.crafts@gmail.com>
2024-08-16 00:09:22 +0900

    feat(): Search Input with Debounce

--- src/components/ui/atom/Input/InputBox.tsx
+++ src/components/ui/atom/Input/InputBox.tsx
@@ -4,59 +4,49 @@ import React, { ChangeEventHandler, FunctionComponent, memo } from 'react';
 
 const sizeSelector: { [keys in inputBoxSizes] } = {
   xsmall: 'h-6 text-xs',
-  small: 'h-12',
-  medium: 'h-14',
-  large: 'h-16',
+  small: 'h-8 text-sm',
+  medium: 'h-12 text-base',
+  large: 'h-16 text-lg',
+};
+
+const widthSelector: { [keys in inputBoxSizes] } = {
+  xsmall: 'w-20',
+  small: 'w-40',
+  medium: 'w-60',
+  large: 'w-80',
 };
 
 const InputBox: FunctionComponent<{
   disabled?: boolean;
   type: 'id' | 'email' | 'password';
   name: string;
-  label: string;
   value: string | number;
   size?: inputBoxSizes;
   placeholder?: string;
   readOnly?: boolean;
-  error?: boolean;
-  errorMessage?: string;
   fullWidth?: boolean;
   classNames?: string;
   onChange?: ChangeEventHandler<HTMLInputElement>;
-}> = ({
-  disabled = false,
-  name,
-  label,
-  size = 'medium',
-  error,
-  errorMessage = 'wrong input',
-  fullWidth = false,
-  classNames,
-  ...props
-}) => {
+}> = ({ disabled = false, name, size = 'medium', fullWidth = false, classNames, ...props }) => {
   return (
-    <div className={fullWidth ? 'w-full' : 'w-[280px] md:w-[320px]'}>
-      <div className="w-full h-full space-y-2">
-        <label htmlFor={name}>
-          <p className="text-xs font-bold md:text-base">{label}</p>
-        </label>
-        <input
-          disabled={disabled}
-          id={name}
-          name={name}
-          className={cx(
-            'p-2 w-full h-full',
-            sizeSelector[size],
-            'border-2 rounded-xl',
-            error ? 'border-red-400' : 'border-primary-600',
-            'focus:outline-none',
-            'disabled:bg-gray-500',
-            classNames
-          )}
-          {...props}
-        />
-      </div>
-      {error && <p className="text-xs md:text-sm text-red-400">{errorMessage}</p>}
+    <div
+      className={cx(
+        classNames,
+        fullWidth ? 'w-full' : widthSelector[size],
+        sizeSelector[size],
+        'border-2 rounded-xl',
+        'overflow-hidden',
+        'focus-within:border-blue-500'
+      )}
+    >
+      <label htmlFor={name} />
+      <input
+        disabled={disabled}
+        id={name}
+        name={name}
+        className={'p-2 w-full h-full no-border-outline'}
+        {...props}
+      />
     </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { PageSEO } from '@src/components/analytics/SEO';
 import { PageLayout } from '@src/components/layout';
-import { Button } from '@src/components/ui/atom';
+import { Button, InputBox } from '@src/components/ui/atom';
 import { RepositoryInfo } from '@src/components/ui/molecule';
 import siteMetadata from '@src/core/config/siteMetadata';
 import { initEnvironment } from '@src/core/lib/relay';
@@ -9,10 +9,11 @@ import repositoryOwnerQuery, {
 } from '@src/core/queries/repositoryOwnerQuery';
 import { ToastError } from '@src/utils/toastUtil';
 import { produce } from 'immer';
-import { useCallback, useRef, useState } from 'react';
+import { Fragment, SyntheticEvent, useCallback, useRef, useState } from 'react';
 import { fetchQuery } from 'react-relay';
 
 const pagination = 5;
+const defaultLogin = 'green-labs';
 
 export async function getStaticProps() {
   const environment = initEnvironment();
@@ -20,7 +21,7 @@ export async function getStaticProps() {
     environment,
     repositoryOwnerQuery,
     {
-      login: 'green-labs',
+      login: defaultLogin,
       first: pagination,
     }
   );
@@ -39,57 +40,114 @@ interface HomePageProps {
 }
 
 function HomePage({ repositoryOwner }: HomePageProps) {
-  const [repositoryInfo, setRepositoryInfo] =
-    useState<RepositoryOwnerQueryResponse['response']['repositoryOwner']>(repositoryOwner);
+  const [searchText, setSearchText] = useState(defaultLogin);
+  const [repoInfo, setRepoInfo] = useState<
+    RepositoryOwnerQueryResponse['response']['repositoryOwner']
+  >(() => repositoryOwner);
+  const { edges } = repoInfo.repositories;
 
-  const { edges } = repositoryInfo.repositories;
-  const lastCursor = useRef<string>(edges[edges.length - 1].cursor);
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+  const lastCursor = useRef<string>(edges.length > 0 ? edges[edges.length - 1].cursor : null);
+
+  const fetchNextPage = useCallback(
+    async (nextSearchText: string) => {
+      const environment = initEnvironment();
+      const queryProps = await fetchQuery<RepositoryOwnerQueryResponse>(
+        environment,
+        repositoryOwnerQuery,
+        {
+          login: nextSearchText,
+          first: pagination,
+          after: lastCursor.current ?? null,
+        }
+      );
+      const _newRepo = { ...queryProps } as RepositoryOwnerQueryResponse['response'];
+      // exception
+      // NotFound
+      if (!_newRepo.repositoryOwner) {
+        lastCursor.current = null;
+        setRepoInfo({ id: null, login: null, repositories: { edges: [] } });
+        return;
+      }
+      // No more data
+      if (
+        repoInfo.id === _newRepo.repositoryOwner.id &&
+        _newRepo.repositoryOwner.repositories.edges.length === 0
+      ) {
+        ToastError('더이상 데이터가 없습니다.');
+        return;
+      }
+      // set cursor
+      lastCursor.current =
+        _newRepo.repositoryOwner.repositories.edges[
+          _newRepo.repositoryOwner.repositories.edges.length - 1
+        ].cursor;
+      // different user or nextSearchText is empty
+      if (repoInfo.id !== _newRepo.repositoryOwner.id) {
+        // set cursor
+
+        setRepoInfo(_newRepo.repositoryOwner);
+        return;
+      } else {
+        setRepoInfo(
+          produce(repoInfo, (draft) => {
+            draft.repositories.edges.push(..._newRepo.repositoryOwner.repositories.edges);
+          })
+        );
+      }
+    },
+    [repoInfo]
+  );
 
   const handleLoadMore = useCallback(async () => {
-    const environment = initEnvironment();
-    const queryProps = await fetchQuery<RepositoryOwnerQueryResponse>(
-      environment,
-      repositoryOwnerQuery,
-      {
-        login: 'green-labs',
-        first: pagination,
-        after: lastCursor.current ?? null,
+    await fetchNextPage(searchText);
+  }, [fetchNextPage, searchText]);
+
+  const handleSearchTextChange = useCallback(
+    (e: SyntheticEvent<HTMLInputElement>) => {
+      const { value } = e.currentTarget;
+
+      setSearchText(value);
+
+      // debounce
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
       }
-    );
-    const _newRepositoryInfo = { ...queryProps } as RepositoryOwnerQueryResponse['response'];
-    // exception
-    if (_newRepositoryInfo.repositoryOwner.repositories.edges.length === 0) {
-      ToastError('더 이상 불러올 데이터가 없습니다.');
-      return;
-    }
-
-    lastCursor.current =
-      _newRepositoryInfo.repositoryOwner.repositories.edges[
-        _newRepositoryInfo.repositoryOwner.repositories.edges.length - 1
-      ].cursor;
-
-    setRepositoryInfo((prev) => ({
-      ...prev,
-      repositories: {
-        ...produce(prev.repositories, (draft) => {
-          draft.edges.push(..._newRepositoryInfo.repositoryOwner.repositories.edges);
-        }),
-      },
-    }));
-  }, []);
+      timeoutRef.current = setTimeout(async () => {
+        fetchNextPage(value);
+      }, 500);
+    },
+    [fetchNextPage]
+  );
 
   return (
     <PageLayout>
       <PageSEO title={siteMetadata.title} description={'Search Github Repositories'} />
-      <div className="w-full space-y-4 divide-y-2">
+      <div className="w-full divide-y-2">
+        <div className="flex py-2 w-4/5 mx-auto">
+          <InputBox
+            type="id"
+            name="searchText"
+            placeholder={'Github Repo'}
+            value={searchText}
+            fullWidth
+            onChange={handleSearchTextChange}
+          />
+        </div>
         {edges.length > 0 ? (
-          edges.map((edge) => <RepositoryInfo key={edge.cursor} {...edge} />)
+          <Fragment>
+            {edges.map((edge) => (
+              <RepositoryInfo key={edge.cursor} {...edge} />
+            ))}
+            <div className="py-2">
+              <Button roundness="counter" onClick={handleLoadMore}>
+                더보기
+              </Button>
+            </div>
+          </Fragment>
         ) : (
           <p className="text-center">No repositories found</p>
         )}
-        <Button roundness="counter" onClick={handleLoadMore}>
-          더보기
-        </Button>
       </div>
     </PageLayout>
   );
